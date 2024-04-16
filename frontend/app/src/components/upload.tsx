b5393ac410d0aa49d0612c63ef953e7d85581475
Blake C <blake.caldwell5@gmail.com>
2024-04-16 15:06:22 +0800

    fade in/ final touch

--- frontend/app/src/components/upload.tsx
+++ frontend/app/src/components/upload.tsx
@@ -38,21 +38,31 @@ export const FileUpload = () => {
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => { // for file browser
     if (e.target.files) {
-      [...e.target.files].forEach((file, i) => {
-        console.log(`File ${i + 1}: ${file.name}`);
-      });
+      // [...e.target.files].forEach((file, i) => {
+      //   console.log(`File ${i + 1}: ${file.name}`);
+      // });
 
       const newFiles = e.target.files;
       const tempFiles: File[] = Array.from(newFiles).filter( // this syntax is wild
         (file) => !selectedFiles.some((selectedFile) => selectedFile.name === file.name)
       );
       setSelectedFiles((prevFiles) => [...prevFiles, ...tempFiles]);
+
+      if (fileInputRef.current) { //used to clear saved state from file browser
+        fileInputRef.current.value = "";
+      }
     }
   };
 
   const handleRemoveFile = (index: number) => {
     setSelectedFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
   }
+
+  // const logFiles = () => {
+  //   [...selectedFiles].forEach((file, i) => {
+  //     console.log(`File ${i + 1}: ${file.name}`);
+  //   })
+  // }
 
   return (
     <div>
@@ -97,33 +107,32 @@ export const FileUpload = () => {
           style={{ display: "none" }}
         />
 
-
+      
       </div>
+      {/* <button onClick={() => logFiles()}>
+        Log Collected Files
+      </button>  USE THIS FOR DEBUGGING*/} 
+      {selectedFiles.length > 0 ? (
 
-      {selectedFiles.length > 0 && (
-
-        <div className="text-white pt-8">
-          <h2 className="text-center text-xl pb-3"> {selectedFiles.length} Selected Files</h2>
+        <div className="text-white pt-8 transition-opacity ease-in duration-700 opacity-100">
+          <h2 className="text-center text-xl pb-3"> {selectedFiles.length} Selected File(s)</h2>
           <ul className="bg-slate-100 rounded-lg">
 
             {selectedFiles.map((file, index) => (
-              <li key={index} className=" flex items-center justify-between text-black px-4 py-2">
+              <li key={index} className=" flex items-center justify-between text-slate-900 px-4 py-2">
                 <span>{file.name}</span>
                 <button
-                  className="text-red-500 hover:text-red-700 focus:outline-none "
+                  className="text-red-500 hover:text-red-700 focus:outline-none hover:scale-105"
                   onClick={() => handleRemoveFile(index)}
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    viewBox="0 30 30"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10.185,1.417c-4.741,0-8.583,3.842-8.583,8.583c0,4.74,3.842,8.582,8.583,8.582S18.768,14.74,18.768,10C18.768,5.259,14.926,1.417,10.185,1.417 M10.185,17.68c-4.235,0-7.679-3.445-7.679-7.68c0-4.235,3.444-7.679,7.679-7.679S17.864,5.765,17.864,10C17.864,14.234,14.42,17.68,10.185,17.68 M10.824,10l2.842-2.844c0.178-0.176,0.178-0.46,0-0.637c-0.177-0.178-0.461-0.178-0.637,0l-2.844,2.841L7.341,6.52c-0.176-0.178-0.46-0.178-0.637,0c-0.178,0.176-0.178,0.461,0,0.637L9.546,10l-2.841,2.844c-0.178,0.176-0.178,0.461,0,0.637c0.178,0.178,0.459,0.178,0.637,0l2.844-2.841l2.844,2.841c0.178,0.178,0.459,0.178,0.637,0c0.178-0.176,0.178-0.461,0-0.637L10.824,10z"
-                      clipRule="evenodd"
-                    />
+                  <svg xmlns="http://www.w3.org/2000/svg" 
+                        className="h-5 w-5" 
+                        viewBox="0 0 20 20"  
+                        fill="currentColor" 
+                        stroke="currentColor" 
+                        stroke-width="1.1">
+
+                    <path fillRule="evenodd" d="M10.185,1.417c-4.741,0-8.583,3.842-8.583,8.583c0,4.74,3.842,8.582,8.583,8.582S18.768,14.74,18.768,10C18.768,5.259,14.926,1.417,10.185,1.417 M10.185,17.68c-4.235,0-7.679-3.445-7.679-7.68c0-4.235,3.444-7.679,7.679-7.679S17.864,5.765,17.864,10C17.864,14.234,14.42,17.68,10.185,17.68 M10.824,10l2.842-2.844c0.178-0.176,0.178-0.46,0-0.637c-0.177-0.178-0.461-0.178-0.637,0l-2.844,2.841L7.341,6.52c-0.176-0.178-0.46-0.178-0.637,0c-0.178,0.176-0.178,0.461,0,0.637L9.546,10l-2.841,2.844c-0.178,0.176-0.178,0.461,0,0.637c0.178,0.178,0.459,0.178,0.637,0l2.844-2.841l2.844,2.841c0.178,0.178,0.459,0.178,0.637,0c0.178-0.176,0.178-0.461,0-0.637L10.824,10z" clipRule="evenodd" />
                   </svg>
                 </button>
               </li>
@@ -131,12 +140,15 @@ export const FileUpload = () => {
           </ul>
 
           <div className="flex justify-center mt-4">
-            <button className="bg-green-300 hover:bg-green-400 rounded-3xl text-lg font-semibold text-slate-900 px-6 py-1 transition-all duration-300 ease-in-out transform hover:scale-110">
+            <button className=" bg-green-400 hover:bg-green-500 rounded-3xl text-lg font-semibold text-slate-900 px-6 py-1 transition-all duration-300 ease-in-out transform hover:scale-110">
               Submit
             </button>
           </div>
         </div>
-      )}
+      ) : (
+        <div className="text-white pt-8 transition-opacity ease-in duration-700 opacity-0">
+          {/* Empty state content */}
+        </div>)}
     </div>
 
 
