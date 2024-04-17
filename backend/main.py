259e97e0a38d3a10f46df348f1969d9eac0467ba
Blake C <blake.caldwell5@gmail.com>
2024-04-17 16:18:39 +0800

    removing vscode folder

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+
+# 'consts'
+UPLOAD_DIR = "uploads"
 
 app = FastAPI()
 
@@ -25,3 +28,11 @@ def index():
 @app.get("/test")
 def test():
     return "Hello from FastAPI"
+
+
+# file uploading
+# this in the future should be session driven. i.e files are stored to a session id. future problem however
+@app.post("/upload_files")
+async def upload_files(files: list[UploadFile]):
+    for file in files:
+        data = await file.read()
