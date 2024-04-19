e74d4f76e72b56bc884107bd6528c02d4b5853c3
Blake C <blake.caldwell5@gmail.com>
2024-04-19 10:38:47 +0800

    error box working

--- frontend/app/src/components/error.tsx
+++ frontend/app/src/components/error.tsx
@@ -1,3 +1,41 @@
 import { useEffect } from 'react';
 
+interface ErrorPopupProps {
+  message: string;
+  onClose: () => void;
+}
+
+const ErrorPopup: React.FC<ErrorPopupProps> = ({ message, onClose }) => {
+  useEffect(() => {
+    const outsideClickCall = (e: MouseEvent) => {
+      const popupContent = document.querySelector('.popup-content');
+      if (popupContent && !popupContent.contains(e.target as Node)) {
+        onClose();
+      }
+    };
+
+    document.addEventListener('mousedown', outsideClickCall);
+
+    return () => {
+      document.removeEventListener('mousedown', outsideClickCall);
+    };
+  }, [onClose]);
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+      <div className="bg-white p-6 flex flex-col items-center justify-center rounded-lg shadow-lg popup-content">
+        <h3 className="text-xl text-black font-semibold mb-4">Error:</h3>
+        <p className="text-lg font-semibold text-red-600 mb-6">{message}</p>
+        <button
+          onClick={onClose}
+          className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPopup;
 // continue here
--- frontend/app/src/utils/api.tsx
+++ frontend/app/src/utils/api.tsx
@@ -1,27 +1,24 @@
 import axios from 'axios'
 
 const api = axios.create(
-    {
-        baseURL: 'http://localhost:8000'
-    }
+  {
+    baseURL: 'http://localhost:8000'
+  }
 )
 
 const uploadFiles = async (files: File[]): Promise<any> => {
-    const formData = new FormData();
-    for (const file of files) {
-      formData.append('files', file);
-    }
-    try {
-      const response = await api.post('/upload_files', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-      return response.data;
-    } catch (error) {
-      console.error('Files failed to upload');
-    }
-  };
+  const formData = new FormData();
+  for (const file of files) {
+    formData.append('files', file);
+  }
 
-  const apiModule = { api, uploadFiles };
-  export default apiModule;
+  const response = await api.post('/upload_files', formData, { //leave the try/catch to outside
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+  return response.data;
+};
+
+const apiModule = { api, uploadFiles };
+export default apiModule;
