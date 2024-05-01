f651365ea7df48f6dfd4815a3c867007241a7690
Blake C <blake.caldwell5@gmail.com>
2024-05-01 17:12:04 +0800

    The 63 integrate backend unit testing (#7)
    * works
    
    * github workflow
    
    * trying to fix fail
    
    * please
    
    * should fix?
    
    * lord save me
    
    * 504th times the charm
    
    * ..
    
    * please

---
+++ backend/UT_main.py
@@ -0,0 +1,43 @@
+# unit lib includes
+import unittest  # see https://docs.python.org/3/library/unittest.html
+from unittest.mock import mock_open, patch
+
+# lib includes
+import os
+import yaml
+
+# our includes
+from src.config.config import Config
+
+
+# CONFIG TESTS
+class TestConfigMethods(unittest.TestCase):
+    def setUp(self):
+        self.file_name = "testing/test.yaml"
+        self.test_data = {
+            "BOOL": True,
+            "STRING": "STRING",
+            "ARRAY": ["Item 1", "Item 2"],
+            "INT": 8000,
+        }
+
+    def test_init(self):
+        config = Config(self.file_name)
+        self.assertEqual(config.FILE_NAME, self.file_name)
+        self.assertEqual(config.data, self.test_data)
+
+    def test_load_data(self):
+        with patch("builtins.open", mock_open(read_data=yaml.dump(self.test_data))):
+            config = Config(self.file_name)
+            self.assertEqual(config.data, self.test_data)
+
+    def test_getitem(self):
+        config = Config(self.file_name)
+        self.assertEqual(config["BOOL"], True)
+        self.assertEqual(config["STRING"], "STRING")
+        self.assertEqual(config["ARRAY"], ["Item 1", "Item 2"])
+        self.assertEqual(config["INT"], 8000)
+
+
+if __name__ == "__main__":
+    unittest.main()
