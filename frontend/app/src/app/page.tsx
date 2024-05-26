fde83085d3a69ef509f0d3d0a5ce52e8908cbd7d
Blake C <blake.caldwell5@gmail.com>
2024-05-26 20:38:47 +0800

    cleanup to fix docker build

--- frontend/app/src/app/page.tsx
+++ frontend/app/src/app/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { FileUpload } from '@/components/upload';
-import Link from 'next/link';
 
 import { Separator } from '@/components/ui/separator';
  
@@ -15,12 +14,6 @@ export default function Home() {
         </h1>
         <Separator className=' mb-5 w-96 h-0.5 bg-gray-200'></Separator>
         <FileUpload></FileUpload>
-
-        {/* <Link href="/test_results">
-          <button className="mt-8 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-            Results test
-          </button>
-        </Link> */}
 
       </div>
     </div>
